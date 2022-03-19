e3354314149a1a092cf920f64310d9fb0e62718d
Sahil Saini <thisissahilsaini@gmail.com>
2022-03-19 12:25:25 +0530

    backend added

---
+++ src/app.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+require("./db/conn");
+const Register = require("./models/registers")
+
+const port = process.env.PORT || 3000;
+
+const static_path = path.join(__dirname, "../public");
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+
+
+app.use(express.static(static_path))
+
+app.get("/", (req, res) =>{
+    res.render("index")
+});
+
+app.get("/register", (req, res) =>{
+    res.render("register")
+});
+
+app.post("/register", async (req, res) =>{
+    try {
+
+        const password = req.body.password;
+        const cpassword = req.body.confirmpassword;
+
+        if(password === cpassword){
+
+            const registerParticipant = new Register({
+                firstname :req.body.firstname, 
+                lastname :req.body.lastname, 
+                email:req.body.email, 
+                phone:req.body.phone, 
+                password:password,
+                confirmpassword:cpassword, 
+                gender:req.body.gender
+            })
+
+           const registered = await registerParticipant.save();
+           res.status(201).render("index");
+
+        }else{
+            res.send("password are not matching")
+        }
+        
+        
+    } catch (error) {
+        res.status(400).send(error)
+    }
+});
+
+// app.get("/", (req, res) =>{
+//     res.send("Hello, Sahil here !")
+// });
+
+app.listen(port, () => {
+    console.log(`server is running at port no ${port}`);
+})
+
---
+++ src/models/registers.js
@@ -0,0 +1,40 @@
+const mongoose =  require("mongoose");
+
+const participantsSchema = new mongoose.Schema({
+    firstname: {
+        type:String,
+        required:true
+    },
+    lastname: {
+        type:String,
+        required:true
+    },
+    email: {
+        type:String,
+        required:true
+    },
+    phone:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    gender:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    confirmpassword:{
+        type:String,
+        required:true
+    }
+
+})
+
+// Collections
+
+const Register = new mongoose.model("Register", participantsSchema)
+
+module.exports = Register;
